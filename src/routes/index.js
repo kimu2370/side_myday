c90a8cb2f69695c82bca237aa3985e0405732a4d
kimu2370 <kimu2370@naver.com>
2021-03-16 22:12:28 +0900

    transition animation WIP

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Main from 'pages/main';
-import Register from 'pages/register';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
+
+import Main from "pages/main";
+import Register from "pages/register";
 
 export default function Routes() {
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" children={<Main />} />
-                <Route path="/register" children={<Register />} />
-            </Switch>
+            <Route
+                render={({location}) => {
+                    console.log(location);
+                    return (
+                        <TransitionGroup>
+                            <CSSTransition key={location.pathname} classNames="page" timeout={400}>
+                                <Switch location={location}>
+                                    <Route exact path="/" children={<Main />} />
+                                    <Route path="/register" children={<Register />} />
+                                </Switch>
+                            </CSSTransition>
+                        </TransitionGroup>
+                    );
+                }}
+            />
         </Router>
     );
 }
